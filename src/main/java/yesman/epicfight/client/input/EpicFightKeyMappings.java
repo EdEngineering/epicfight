d4ab7678d337e9d33f7c4cca4049bed22247de37
Alfonso <78657787+EdEngineering@users.noreply.github.com>
2025-08-18 05:29:43 +0000

    keymapping: public class

--- src/main/java/yesman/epicfight/client/input/EpicFightKeyMappings.java
+++ src/main/java/yesman/epicfight/client/input/EpicFightKeyMappings.java
@@ -11,18 +11,17 @@ import yesman.epicfight.main.EpicFightMod;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, bus=Mod.EventBusSubscriber.Bus.MOD)
 public class EpicFightKeyMappings {
-	private EpicFightKeyMappings(){}
 	public static final KeyMapping WEAPON_INNATE_SKILL_TOOLTIP = new KeyMapping("key." + EpicFightMod.MODID + ".show_tooltip", InputConstants.KEY_LSHIFT, "key." + EpicFightMod.MODID + ".gui");
 	public static final KeyMapping SWITCH_MODE = new KeyMapping("key." + EpicFightMod.MODID + ".switch_mode", InputConstants.KEY_R, "key." + EpicFightMod.MODID + ".combat");
 	public static final KeyMapping DODGE = new KeyMapping("key." + EpicFightMod.MODID + ".dodge", InputConstants.KEY_LALT, "key." + EpicFightMod.MODID + ".combat");
-	public static final KeyMapping GUARD = new KeyMapping("key." + EpicFightMod.MODID + ".guard", InputConstants.KEY_LALT, "key." + EpicFightMod.MODID + ".combat");
+	public static final KeyMapping GUARD = new KeyMapping("key." + EpicFightMod.MODID + ".guard", InputConstants.Type.MOUSE, 1, "key." + EpicFightMod.MODID + ".combat");
 	public static final KeyMapping ATTACK = new  CombatKeyMapping("key." + EpicFightMod.MODID + ".attack", InputConstants.Type.MOUSE, 0, "key." + EpicFightMod.MODID + ".combat");
 	public static final KeyMapping WEAPON_INNATE_SKILL = new CombatKeyMapping("key." + EpicFightMod.MODID + ".weapon_innate_skill", InputConstants.Type.MOUSE, 0, "key." + EpicFightMod.MODID + ".combat");
 	public static final KeyMapping MOVER_SKILL = new  CombatKeyMapping("key." + EpicFightMod.MODID + ".mover_skill", InputConstants.Type.KEYSYM, InputConstants.KEY_SPACE, "key." + EpicFightMod.MODID + ".combat");
 	public static final KeyMapping SKILL_EDIT = new KeyMapping("key." + EpicFightMod.MODID + ".skill_gui", InputConstants.KEY_K, "key." + EpicFightMod.MODID + ".gui");
 	public static final KeyMapping LOCK_ON = new KeyMapping("key." + EpicFightMod.MODID + ".lock_on", InputConstants.KEY_G, "key." + EpicFightMod.MODID + ".combat");
 	public static final KeyMapping CONFIG = new KeyMapping("key." + EpicFightMod.MODID + ".config", -1, "key." + EpicFightMod.MODID + ".gui");
-
+	
 	@SubscribeEvent
 	public static void registerKeys(RegisterKeyMappingsEvent event) {
 		event.register(WEAPON_INNATE_SKILL_TOOLTIP);
